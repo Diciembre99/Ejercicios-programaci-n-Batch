8503dced8dadd31870d8724be5f2c3e993d755f8
Diciembre99 <118851645+Diciembre99@users.noreply.github.com>
2023-01-05 16:42:48 +0100

    Update Ejercicio5.bat

--- Ejercicio5.bat
+++ Ejercicio5.bat
@@ -1,37 +1,45 @@
 @echo off
-:menu
-echo ********************************
-echo ****  Menu de opciones    ******
-echo **** 1.Agregar Contactos  ******
-echo **** 2.Mostrar Contactos  ******
-echo **** 3.Buscar Contactos   ******
-echo **** 4.  Salir            ******
-echo ********************************
-echo.
-set /p opc="Escribe la opcion que quieras: "
-IF %opc% EQU 1 (
-  set /p nombre="Escribe el nombre del contacto: "
-  set /p apellido="Escribe los apellidos del contacto: "
-  set /p edad="Escribe la edad del contacto: "
-  goto nombre
-
-  pause>nul
-  REM Agregar
-)
-:nombre
-%nombre%>contactos.txt
-REM IF opc EQU 2 (
-REM   goto mostrar
-REM   REM mostar
-REM )
-REM IF opc EQU 3 (
-REM   goto buscar
-REM   REM buscar
-REM )
-REM IF opc EQU 4 (
-REM   goto salir
-REM   REM Salir
-REM )
-pause>nul
 goto menu
-exit
+:agregar_contacto
+echo Ingrese el nombre del contacto:
+set /p nombre=
+echo Ingrese los apellidos del contacto:
+set /p apellidos=
+echo Ingrese la edad del contacto:
+set /p edad=
+echo %nombre% - %apellidos% - %edad% >> agenda.txt
+echo Contacto agregado correctamente.
+pause
+goto menu
+
+:mostrar_contactos
+type agenda.txt
+pause
+goto menu
+
+:buscar_contactos
+echo Ingrese el nombre del contacto a buscar:
+set /p nombre=
+find /i "%nombre%" agenda.txt > resultados.txt
+if %ERRORLEVEL% == 0 (
+  type resultados.txt
+) else (
+  echo No se ha encontrado ningún contacto con ese nombre.
+)
+del resultados.txt
+pause
+goto menu
+
+:menu
+cls
+echo Seleccione una opcion:
+echo 1. Agrega Contacto
+echo 2. Mostrar Contactos
+echo 3. Buscar Contactos
+set /p opcion=
+if %opcion% == 1 goto agregar_contacto
+if %opcion% == 2 goto mostrar_contactos
+if %opcion% == 3 goto buscar_contactos
+echo Opcion invalida.
+pause
+goto menu
